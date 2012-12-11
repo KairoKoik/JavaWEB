63ca71e78e4c8a5ea27bc0ad16e3a971b7c2734d
Marek Kikas <marekk657@gmail.com>
2012-12-11 23:01:24 +0200

    JPA ja entityd

--- Projekt/src/i377/entities/PiirivalvurVaeosas.java
+++ Projekt/src/i377/entities/PiirivalvurVaeosas.java
@@ -1,5 +1,96 @@
 package i377.entities;
 
-public class PiirivalvurVaeosas {
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the PIIRIVALVURVAEOSAS database table.
+ * 
+ */
+@Entity
+public class PiirivalvurVaeosas implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private long id;
+
+	private String kommentaar;
+
+	private double koormus;
+
+	private int version;
+
+	//bi-directional many-to-one association to AmetVaeosas
+	@ManyToOne
+	@JoinColumn(name="AMETVAEOSAS_ID")
+	private AmetVaeosas ametvaeosa;
+
+	//bi-directional many-to-one association to Piirivalvur
+	@ManyToOne
+	private Piirivalvur piirivalvur;
+
+	//bi-directional many-to-one association to Vaeosa
+	@ManyToOne
+	private Vaeosa vaeosa;
+
+	public PiirivalvurVaeosas() {
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getKommentaar() {
+		return this.kommentaar;
+	}
+
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+
+	public double getKoormus() {
+		return this.koormus;
+	}
+
+	public void setKoormus(double koormus) {
+		this.koormus = koormus;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public AmetVaeosas getAmetvaeosa() {
+		return this.ametvaeosa;
+	}
+
+	public void setAmetvaeosa(AmetVaeosas ametvaeosa) {
+		this.ametvaeosa = ametvaeosa;
+	}
+
+	public Piirivalvur getPiirivalvur() {
+		return this.piirivalvur;
+	}
+
+	public void setPiirivalvur(Piirivalvur piirivalvur) {
+		this.piirivalvur = piirivalvur;
+	}
+
+	public Vaeosa getVaeosa() {
+		return this.vaeosa;
+	}
+
+	public void setVaeosa(Vaeosa vaeosa) {
+		this.vaeosa = vaeosa;
+	}
 
 }
--- Projekt/src/i377/entities/Vaeosa.java
+++ Projekt/src/i377/entities/Vaeosa.java
@@ -1,5 +1,94 @@
 package i377.entities;
 
-public class Vaeosa {
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the VAEOSA database table.
+ * 
+ */
+@Entity
+public class Vaeosa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private long id;
+
+	private String kommentaar;
+
+	private String kood;
+
+	private String nimetus;
+
+	private int version;
+
+	//bi-directional many-to-one association to AmetVaeosas
+	@OneToMany(mappedBy="vaeosa")
+	private List<AmetVaeosas> ametvaeosas;
+
+	//bi-directional many-to-one association to PiirivalvurVaeosas
+	@OneToMany(mappedBy="vaeosa")
+	private List<PiirivalvurVaeosas> piirivalvurvaeosas;
+
+	public Vaeosa() {
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getKommentaar() {
+		return this.kommentaar;
+	}
+
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+
+	public String getKood() {
+		return this.kood;
+	}
+
+	public void setKood(String kood) {
+		this.kood = kood;
+	}
+
+	public String getNimetus() {
+		return this.nimetus;
+	}
+
+	public void setNimetus(String nimetus) {
+		this.nimetus = nimetus;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public List<AmetVaeosas> getAmetvaeosas() {
+		return this.ametvaeosas;
+	}
+
+	public void setAmetvaeosas(List<AmetVaeosas> ametvaeosas) {
+		this.ametvaeosas = ametvaeosas;
+	}
+
+	public List<PiirivalvurVaeosas> getPiirivalvurvaeosas() {
+		return this.piirivalvurvaeosas;
+	}
+
+	public void setPiirivalvurvaeosas(List<PiirivalvurVaeosas> piirivalvurvaeosas) {
+		this.piirivalvurvaeosas = piirivalvurvaeosas;
+	}
 
 }
