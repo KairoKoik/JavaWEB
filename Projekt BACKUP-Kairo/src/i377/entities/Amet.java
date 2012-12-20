41cd40b7d9f72c5fb7fb581f900e99e51f628269
Kairo Koik <kairokoik@gmail.com>
2012-12-20 03:41:11 +0200

    Tegin backupi vanast projektist (just in case) ja lisasin 4 JSP
    Homme lähen kool ja teen viimased 2 ka ära, kuna datepicker ei taha minu
    arvutis tööle hakkata.

---
+++ Projekt BACKUP-Kairo/src/i377/entities/Amet.java
@@ -0,0 +1,82 @@
+package i377.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the AMET database table.
+ * 
+ */
+@Entity
+public class Amet implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private long id;
+
+	private String iscokood;
+
+	private String kommentaar;
+
+	private String nimetus;
+
+	private int version;
+
+	//bi-directional many-to-one association to AmetVaeosas
+	@OneToMany(mappedBy="amet")
+	private List<AmetVaeosas> ametvaeosas;
+
+	public Amet() {
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getIscokood() {
+		return this.iscokood;
+	}
+
+	public void setIscokood(String iscokood) {
+		this.iscokood = iscokood;
+	}
+
+	public String getKommentaar() {
+		return this.kommentaar;
+	}
+
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+
+	public String getNimetus() {
+		return this.nimetus;
+	}
+
+	public void setNimetus(String nimetus) {
+		this.nimetus = nimetus;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public List<AmetVaeosas> getAmetvaeosas() {
+		return this.ametvaeosas;
+	}
+
+	public void setAmetvaeosas(List<AmetVaeosas> ametvaeosas) {
+		this.ametvaeosas = ametvaeosas;
+	}
+
+}
---
+++ Projekt BACKUP-Kairo/src/i377/entities/Piirivalvur.java
@@ -0,0 +1,142 @@
+package i377.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the PIIRIVALVUR database table.
+ * 
+ */
+@Entity
+public class Piirivalvur implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private long id;
+
+	private String aadress;
+
+	private String eesnimi;
+
+	private String email;
+
+	private String isikukood;
+
+	private String kommentaar;
+
+	private String perekonnanimi;
+
+	private String sodurikood;
+
+	private int sugu;
+
+	private String telefon;
+
+	private int version;
+
+	//bi-directional many-to-one association to PiirivalvurVaeosas
+	@OneToMany(mappedBy="piirivalvur")
+	private List<PiirivalvurVaeosas> piirivalvurvaeosas;
+
+	public Piirivalvur() {
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAadress() {
+		return this.aadress;
+	}
+
+	public void setAadress(String aadress) {
+		this.aadress = aadress;
+	}
+
+	public String getEesnimi() {
+		return this.eesnimi;
+	}
+
+	public void setEesnimi(String eesnimi) {
+		this.eesnimi = eesnimi;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIsikukood() {
+		return this.isikukood;
+	}
+
+	public void setIsikukood(String isikukood) {
+		this.isikukood = isikukood;
+	}
+
+	public String getKommentaar() {
+		return this.kommentaar;
+	}
+
+	public void setKommentaar(String kommentaar) {
+		this.kommentaar = kommentaar;
+	}
+
+	public String getPerekonnanimi() {
+		return this.perekonnanimi;
+	}
+
+	public void setPerekonnanimi(String perekonnanimi) {
+		this.perekonnanimi = perekonnanimi;
+	}
+
+	public String getSodurikood() {
+		return this.sodurikood;
+	}
+
+	public void setSodurikood(String sodurikood) {
+		this.sodurikood = sodurikood;
+	}
+
+	public int getSugu() {
+		return this.sugu;
+	}
+
+	public void setSugu(int sugu) {
+		this.sugu = sugu;
+	}
+
+	public String getTelefon() {
+		return this.telefon;
+	}
+
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public List<PiirivalvurVaeosas> getPiirivalvurvaeosas() {
+		return this.piirivalvurvaeosas;
+	}
+
+	public void setPiirivalvurvaeosas(List<PiirivalvurVaeosas> piirivalvurvaeosas) {
+		this.piirivalvurvaeosas = piirivalvurvaeosas;
+	}
+
+}
